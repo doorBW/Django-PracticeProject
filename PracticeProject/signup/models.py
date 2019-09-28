4cda747323b9b0d9cc51c45b0139e8de6da3dd8b
doorBW <doorbw@outlook.com>
2019-09-28 15:41:41 +0900

    step 1

--- PracticeProject/signup/models.py
+++ PracticeProject/signup/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.CharField(unique=True, max_length=200)
+    user_pw = models.TextField()
+    user_name = models.CharField(max_length=30)
+    user_validation = models.CharField(default='0',max_length=5)
